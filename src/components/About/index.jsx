441e160ee518a7cd3ab22390a8e7f908dbe4dc13
AngadBatth <andyybatth@gmail.com>
2023-08-07 11:28:32 -0400

    fixed issue with footer icons not showing

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -7,14 +7,14 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <img src={profilePic} alt="picture of my cat" style={{ width: "130px", height: "130px" }}></img>
+        <img src={profilePic} alt="picture of my cat" style={{ width: "200px", height: "200px" }}></img>
         </div>
         <p>
         Hello! My name is Angad Batth and I am a Fullstack Developer who is currently enrolled in a coding bootcamp hosted by the University of Toronto!
         </p>
         <p>
         Some background about me is that I attended Seneca College for Computer Science in 2017 and soon after moved to the great state of Texas! 
-        During my time in Texas I was able to work with a Tech Contracting company that gave me the opportunity to work with other companies such as 
+        During my time in Texas I was able to work with an IT Contracting company that gave me the opportunity to work alongside other companies such as 
         Raytheon, Boeing and Samsung. Working there sparked a hidden passion I had for coding and so I decided to spend my time learning about Full-Stack Development.
         </p>
       </div>
